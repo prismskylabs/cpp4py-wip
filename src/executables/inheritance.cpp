6e426f675059b7612abe25d89600d9e4b0750f6e
Huu Nguyen <whoshuu@gmail.com>
2016-10-07 15:27:25 -0700

    Add python version of inheritance example

--- src/executables/inheritance.cpp
+++ src/executables/inheritance.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 
+/* Python Inheritance and Polymorphism
+ *
+ * class Animal(object):
+ *     def speak(self):
+ *         raise NotImplementedError()
+ *
+ *     def speak_twice(self):
+ *         self.speak()
+ *         self.speak()
+ *
+ * class Dog(Animal):
+ *     def speak(self):
+ *         print "Ruff!"
+ *
+ * class Cat(Animal):
+ *     def speak(self):
+ *         print "Meow!"
+ *
+ * class Hokie(Animal):
+ *     def speak(self):
+ *         print "Gobble!"
+ *
+ * 
+ * animals = [Dog(), Cat(), Hokie()]
+ *
+ * for animal in animals:
+ *     animal.speak_twice()
+ *
+ */
+
 int main() {
     std::cout << "Hello world" << std::endl;
 }
