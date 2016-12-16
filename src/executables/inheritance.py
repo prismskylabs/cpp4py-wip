846fb908028d295a93fd2bb95e34adbcd6e9e5d0
Huu Nguyen <whoshuu@gmail.com>
2016-12-16 13:51:15 -0800

    Add inheritance.py example

---
+++ src/executables/inheritance.py
@@ -0,0 +1,25 @@
+class Animal(object):
+    def speak(self):
+        raise NotImplementedError()
+
+    def speak_twice(self):
+        self.speak()
+        self.speak()
+
+class Dog(Animal):
+    def speak(self):
+        print "Ruff!"
+
+class Cat(Animal):
+    def speak(self):
+        print "Meow!"
+
+class Hokie(Animal):
+    def speak(self):
+        print "Gobble!"
+
+
+animals = [Dog(), Cat(), Hokie()]
+
+for animal in animals:
+    animal.speak_twice()
